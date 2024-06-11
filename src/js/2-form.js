96fc2d7b1890f72398b8f357f5aadd21f0f2766d
Nataliia Babina <157317342+Nataliia-Babina@users.noreply.github.com>
2024-06-11 20:26:18 +0200

    HW-js-9

---
+++ src/js/2-form.js
@@ -0,0 +1,69 @@
+const STORAGE_KEY = 'feedback-msg';
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+const input = form.querySelector('input');
+const textarea2 = document.querySelector('textarea');
+
+
+form.addEventListener('input', () => {
+    const formData = new FormData(form);
+    const email = formData.get('email');
+    const message = formData.get('message');
+    const data = { email, message }; 
+
+    saveToLS('email', email);
+    saveToLS('message', message);
+    saveToLS('userData', data);
+}); 
+
+function saveToLS(key, value) {
+    const jsonData = JSON.stringify(value);
+    localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key) {
+    const json = localStorage.getItem(key);
+    try {
+        const data = JSON.parse(json);
+        return data;
+    } catch {
+        return json;
+    }
+}  
+
+function claimForm() {
+    if (textarea2.value === '' || input.value === '' ) {
+        return false;
+    } else {
+        return true;
+    }
+    
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const email = formData.get('email');
+    const message = formData.get('message');
+    const data = { email, message }; 
+
+    console.log(data);
+
+    form.reset();
+    localStorage.removeItem('email');
+    localStorage.removeItem('message');
+    localStorage.removeItem('userData');
+
+    if (!claimForm()) {
+        e.preventDefault();
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const email = loadFromLS('email');
+    const message = loadFromLS('message');
+
+    form.elements.email.value = email;
+    form.elements.message.value = message;
+});
